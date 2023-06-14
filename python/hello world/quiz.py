e8a121cd825ac6682bb27b9bd50648b466237df0
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-06-14 14:42:24 -0600

    Update quiz.py

--- python/hello world/quiz.py
+++ python/hello world/quiz.py
@@ -3,9 +3,20 @@
 Score = 0
 
 # Q1
-answer1 = str(input("who is your teacher? "))
-if answer1 == "mr veldkamp":
+answer1 = input("who is your teacher? ")
+if answer1 == "mr veldkamp" or answer1 == "Mr.v" :
     print("correct")
     Score = Score + 1
 else:
     print("incorrect")
+
+#Q2
+answer2 = input("24+26 = ? ")
+if answer2 == "50":
+    print("correct")
+    Score = Score + 1
+else:
+    print("incorrect")
+
+#output score 
+print("your score is" , Score/2 * 100 , "%.")
