b483ce891af16ece970aec9a39ad56c9ee9c8e6d
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-06-14 15:33:24 -0600

    Update dist_calc.py

--- python/hello world/dist_calc.py
+++ python/hello world/dist_calc.py
@@ -4,6 +4,6 @@ num1y = float(input('1y = '))
 num2x = float(input('2x = '))
 num2y = float(input('2y = '))
 
-d = ((num2x - num1x)**2 + (num2y - num2y)**2)**.5
+d = ((num2x - num1x)**2 + (num2y - num1y)**2)**.5
 
-print("Distance =  ", d,".")
+print("Distance =  ", d)
