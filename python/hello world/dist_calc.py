53efaffa470a23098611f7e2aac9e19f7c8e01de
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-06-14 14:49:40 -0600

    Create dist_calc.py

---
+++ python/hello world/dist_calc.py
@@ -0,0 +1,9 @@
+# calculator
+num1x = float(input('1x = '))
+num1y = float(input('1y = '))
+num2x = float(input('2x = '))
+num2y = float(input('2y = '))
+
+d = ((num2x - num1x)**2 + (num2y - num2y)**2)**.5
+
+print("Distance =  ", d,".")
