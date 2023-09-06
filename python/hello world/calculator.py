641c0f93eb7011b8051d12c435379b93abe7b39a
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-09-06 14:51:24 -0600

    Update calculator.py

--- python/hello world/calculator.py
+++ python/hello world/calculator.py
@@ -1,7 +1,16 @@
 # calculator
+'''
 num1 = float(input('x1 = '))
 num2 = float(input('x2 = '))
 
 
 answer = num1 + num2
-print("sum = " + str(answer) + ".")
+print("sum = " + str(answer))
+'''
+import re
+
+equation = str(input('please enter your Equation'))
+split_expression = re.findall()
+
+def decoder():
+
