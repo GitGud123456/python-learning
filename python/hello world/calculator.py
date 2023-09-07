30cb6bef9fc33d7d654d579dcd583198ab786da0
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-09-07 14:51:36 -0600

    Update calculator.py

--- python/hello world/calculator.py
+++ python/hello world/calculator.py
@@ -1,6 +1,8 @@
 import re
 equation = input("please enter your Equation ")
 
+
+
 pattern1 = r'\(.*?\)'  #Brackets
 pattern2 = r'\d*\d*\d\*\*\d\d*'  #Exponets 
 pattern3 = r'\.*\d\.*\d*\.*\d*\d*'  #isolate numbers from symbols
@@ -12,16 +14,12 @@ pattern8 = r'\+|\-'  # look at operator (+,-)
 pattern9 = r'\(|\)'  # look at brackets "(",")"
 pattern10 = r'\*\*'  # look at brackets "(",")"
 
-def doublecheck(type,input):
-    if type == 0:
-        Bedmas(input)
+brackets_done = False
+exponets_done = False
+mult_div_done = False
+add_sub_done = False
 
-    elif type == 1:
-        bEdmas(input)
-    elif type == 2:
-        beDMas(input)
-    elif type == 3:
-        bedmAS(input)
+
 
 def find_innermost_brackets(expression):
     stack = []
@@ -42,7 +40,9 @@ def find_innermost_brackets(expression):
 def Bedmas(var):
     test = re.findall(pattern9,var)
     if len(test) == 0:
-        bEdmas(var)
+        global brackets_done
+        brackets_done = True
+        return var
     else:
         bracket_list = find_innermost_brackets(var)
         start = int(bracket_list[0][0])
@@ -50,12 +50,16 @@ def Bedmas(var):
         isolatedB = var[start:end+1]
         #print(var[start+1:end]
         print(isolatedB)
-        bEdmas(isolatedB)
+        #bEdmas(isolatedB)
+        return isolatedB
 
 def bEdmas(var2):
     test = re.findall(pattern10,var2)
     if len(test) == 0:
-        beDMas(var2)
+        global exponets_done
+        exponets_done = True
+       # beDMas(var2)
+        return var2
     else:
         brokenpart2 = re.findall(pattern2,var2)
         isolatedE = ''.join(brokenpart2)
@@ -71,7 +75,9 @@ def bEdmas(var2):
 def beDMas(var3):
     test = re.findall(pattern6,var3)
     if len(test) == 0:
-        bedmAS(var3)
+        global mult_div_done
+        mult_div_done = True
+       # bedmAS(var3)
     else:
         brokenMD = re.findall(pattern5,var3)
         isolatedMD = ''.join(brokenMD)
@@ -86,19 +92,50 @@ def beDMas(var3):
         bedmAS(updatedEquation)
 
 def bedmAS(var4):
-    brokenAS = re.findall(pattern7,var4)
-    isolatedAS = ''.join(brokenAS)
-    sign = re.findall(pattern8,isolatedAS)
-    [x1,x2] = re.findall(pattern3,isolatedAS)
-    if sign == ['+']:
-        sum3 = float(x1) + float(x2)
+    test = re.findall(pattern8,var4)
+    if len(test) == 0:
+        global add_sub_done
+        add_sub_done = True
+        return var4
     else:
-        sum3 = float(x1) - float(x2)
-    updatedEquation = var4.replace(isolatedAS,str(sum3))
-    print(updatedEquation)
-    return updatedEquation
+        brokenAS = re.findall(pattern7,var4)
+        isolatedAS = ''.join(brokenAS)
+        sign = re.findall(pattern8,isolatedAS)
+        [x1,x2] = re.findall(pattern3,isolatedAS)
+        if sign == ['+']:
+            sum3 = float(x1) + float(x2)
+        else:
+            sum3 = float(x1) - float(x2)
+        updatedEquation = var4.replace(isolatedAS,str(sum3))
+        print(updatedEquation)
+        return updatedEquation
 
-Bedmas(equation)
 
+
+def loop_till_done(b_done,e_done,dm_done,as_done,start_equation):
+    current_equation = Bedmas(start_equation)
+    if b_done == True & e_done == False:
+        current_equation = bEdmas(current_equation)
+    elif e_done == True & mult_div_done == False:
+        current_equation = beDMas(current_equation)
+    elif mult_div_done == True & as_done == False:
+        current_equation = bedmAS(current_equation)
+    elif b_done == True & e_done == True & dm_done == True & as_done == True:
+        print("Done!", current_equation)
+        return False
+
+    return True
+
+while loop_till_done:
+    loop_till_done(brackets_done,exponets_done,mult_div_done,add_sub_done,equation)
+
+
+#Bedmas(equation)
+
+'''
+if brackets_done == True:
+    print("working")
+else:
+    print("fuck")
  
-
+'''
