e23407fea3836832e441bdab160f425fb66bea29
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-09-11 14:53:19 -0600

    Update AI.py

--- python/hello world/AI.py
+++ python/hello world/AI.py
@@ -35,6 +35,57 @@ if a: # evaluates to True
 '''
 
 # Import a library of functions called 'pygame'
+import math
 import pygame
 # Initialize the game engine
 pygame.init()
+
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+
+#Opening and setting the window size
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+
+#Setting the window title
+pygame.display.set_caption("Professor Craven's Cool Game")
+
+# Loop until the user clicks the close button.
+done = False
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            print("User asked to quit.")
+            done = True # Flag that we are done so we exit this loop
+            
+        elif event.type == pygame.KEYDOWN:
+                print("User pressed a key.")
+        elif event.type == pygame.KEYUP:
+                print("User let go of a key.")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+                print("User pressed a mouse button")
+    print("escaped")
+    # --- Game logic should go here
+       
+    # --- Drawing code should go here
+ 
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+ 
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
