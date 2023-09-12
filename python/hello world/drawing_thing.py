622b37becefc22782cd07bdbff0dc25be639f3a9
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-09-12 14:53:21 -0600

    Create drawing_thing.py

---
+++ python/hello world/drawing_thing.py
@@ -0,0 +1,133 @@
+"""
+ Pygame base template for opening a window
+ 
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+ 
+ Explanation video: http://youtu.be/vRB_983kUMc
+"""
+ 
+import pygame
+import math
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+objects_list = []
+
+def actionDetection():
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            print("User asked to quit.")
+            pygame.quit()
+            return True
+        elif event.type == pygame.KEYDOWN:
+            print("User pressed a key.")
+        elif event.type == pygame.KEYUP:
+            print("User let go of a key.")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            drawground(True)
+            print("User pressed a mouse button")
+    return False
+
+
+def drawground(T_F):
+    [x,y] = mouseLocation_finder()
+    pygame.draw.rect(screen, GREEN, [x, y, 100, 10], 0)
+    pygame.draw.rect(screen, BLACK, [x, y+10, 100, 20], 0)
+    if T_F == True:
+        objects_list.append([screen, GREEN, [x, y, 100, 10]])
+def mouseLocation_finder():
+    mouse_pos = pygame.mouse.get_pos() 
+    return mouse_pos
+
+
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("My Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+
+    done == actionDetection()  # Flag that we are done so we exit this loop
+    #mouseLocation_finder()   
+    # --- Game logic should go here
+    
+    # --- Screen-clearing code goes here
+    
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+ 
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(WHITE)
+ 
+    # --- Drawing code should go here
+    #pygame.draw.rect(Surface, color, Rect, width=0): return Rect
+    pygame.draw.rect(screen, RED, [55, 50, 20, 25], 0)
+    # Draw on the screen a green line from (0, 0) to (100, 100)
+    # that is 5 pixels wide.
+    pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+    # Draw on the screen several lines from (0,10) to (100,110)
+    # 5 pixels wide using a for loop
+    for y_offset in range(0, 100, 10):
+        pygame.draw.line(screen,RED,[0,10+y_offset],[100,110+y_offset],5)
+
+    drawground(False)
+ 
+    for item in objects_list:
+        print(item)
+        #pygame.draw.rect(screen, GREEN, [item[0], y, 100, 10], 0)
+    for i in range(200):
+    
+        radians_x = i / 20
+        radians_y = i / 6
+    
+        x = int(75 * math.sin(radians_x)) + 200
+        y = int(75 * math.cos(radians_y)) + 200
+    
+        pygame.draw.line(screen, BLACK, [x,y], [x+5,y], 5)
+
+    # This draws a triangle using the polygon command
+    pygame.draw.polygon(screen, BLACK, [[100,100], [0,200], [200,200]], 5)
+    # Draw an ellipse, using a rectangle as the outside boundaries
+    pygame.draw.ellipse(screen, BLACK, [20,20,250,100], 2)  
+    # Select the font to use, size, bold, italics
+    font = pygame.font.SysFont('Calibri', 25, True, False)
+    
+    # Render the text. "True" means anti-aliased text.
+    # Black is the color. The variable BLACK was defined
+    # above as a list of [0, 0, 0]
+    # Note: This line creates an image of the letters,
+    # but does not put it on the screen yet.
+    text = font.render("My text",True,BLACK)
+    
+    # Put the image of the text on the screen at 250x250
+    screen.blit(text, [250, 250])
+
+
+
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+pygame.quit()
