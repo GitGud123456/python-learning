789d8b811427da45eb8a79d6c1b3a0ca60edd36b
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-10-06 14:53:16 -0600

    Update auto_clicker.py

--- python/hello world/auto_clicker.py
+++ python/hello world/auto_clicker.py
@@ -1,26 +1,43 @@
 from pynput.keyboard import Key, Controller, Listener
 import time
 import pythoncom
-import pyWinhook as pyHook
-
+import pyWinhook
+global key_to_press,repeat_times,delay_between_press,time_till_stop
 keyboard = Controller()
-key_to_press = "Return"
-repeat_times = 50
+key_to_press = 13
+repeat_times = 500
 delay_between_press = .01
-time_till_stop = 4
+time_till_stop = 100000
+
+def check_if_Q(event):
+    if event.Key == "Q":
+        press_enter(repeat_times,delay_between_press,timer)
+
+
+def press_enter(num_presses, delay, duration):
+    start_time = time.time()
+    end_time = start_time + duration
+
+    while time.time() < end_time:
+        for _ in range(num_presses):
+            keyboard.press('enter')
+            keyboard.release('enter')
+            time.sleep(delay)
 
 def loop(key,times,delay,timer):
-
+    print("hi")
+    num = 0
     while num < times: 
-        keyboard.press(key)
-        keyboard.release(key)
+        keyboard.press(13)
+        keyboard.release(13)
         time.sleep(delay)
+        num +=1
     time.sleep(timer)
     return True
 
 #loop(key_to_press,repeat_times,delay_between_press,time_till_stop)
 
-
+'''
 def OnKeyboardEvent(event):
     print('MessageName:',event.MessageName)
     print('Message:',event.Message)
@@ -29,9 +46,6 @@ def OnKeyboardEvent(event):
     print('WindowName:',event.WindowName)
     print('Ascii:', event.Ascii, chr(event.Ascii))
     print('Key:', event.Key)
-    print(type(event,event.Key))
-    if event.Key == "Q":
-        loop(key_to_press,repeat_times,delay_between_press,time_till_stop)
     print('KeyID:', event.KeyID)
     print('ScanCode:', event.ScanCode)
     print('Extended:', event.Extended)
@@ -42,14 +56,14 @@ def OnKeyboardEvent(event):
 
 # return True to pass the event to other handlers
     return True
-
+'''
 # create a hook manager
-hm = pyHook.HookManager()
+hm = pyWinhook.HookManager()
+
 # watch for all mouse events
-hm.KeyDown = OnKeyboardEvent
+#hm.KeyDown = OnKeyboardEvent
+hm.KeyDown = check_if_Q
 # set the hook
 hm.HookKeyboard()
 # wait forever
 pythoncom.PumpMessages()
-
-qqqqqaaaaqqq
