c1a175bfa0d211282793f9b1544b3b49adafb189
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-10-11 14:53:22 -0600

    Update screenCapture.py

--- python/hello world/screenCapture/screenCapture.py
+++ python/hello world/screenCapture/screenCapture.py
@@ -1,16 +1,28 @@
 import time
 import pyautogui
-seconds = time.time()
+import os
+import glob
+global folder_path
+folder_path = 'F:\GitHub\python-learning\python\hello world\screenCapture\ScreenShots'
 
-local_time = time.ctime(seconds)
-
-print("Local time:", local_time)
-
-
-
-#if program running
+file_list = glob.glob(os.path.join(folder_path,'*'))
 def takeScreenShot():
     ScreenShot = pyautogui.screenshot()
-    ScreenShot.save('f:\GitHub\python-learning\screenCapture\ScreenShots.data')
+    ScreenShot.save(f'{folder_path}\ScreenShot.png')
+
 
 takeScreenShot()
+
+def watch(delay,boolean):
+    while boolean:
+        takeScreenShot()
+        if len(file_list) >= 100:
+            file_list.sort(key=os.path.getmtime)
+            os.remove(file_list[0])
+            print("removed")
+        else:
+            print("not removed")
+    time.sleep(delay)
+
+watch(0,True)
+
