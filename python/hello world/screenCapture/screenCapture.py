4cd7aba670008a8cf7efc008323e508cea67f9a1
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-10-12 14:53:43 -0600

    Update screenCapture.py

--- python/hello world/screenCapture/screenCapture.py
+++ python/hello world/screenCapture/screenCapture.py
@@ -1,19 +1,20 @@
-import time
+'''import time
 import pyautogui
 import os
 import glob
-global folder_path
-folder_path = 'F:\GitHub\python-learning\python\hello world\screenCapture\ScreenShots'
+global folder_path,file_list
 
+folder_path = 'F:\GitHub\python-learning\python\hello world\screenCapture\ScreenShots'
 file_list = glob.glob(os.path.join(folder_path,'*'))
+
+
 def takeScreenShot():
     ScreenShot = pyautogui.screenshot()
-    ScreenShot.save(f'{folder_path}\ScreenShot.png')
+    ScreenShot.save(f'{folder_path}\ScreenShot{len(file_list)+1}.png')
 
-
-takeScreenShot()
 
 def watch(delay,boolean):
+    file_list = glob.glob(os.path.join(folder_path,'*'))
     while boolean:
         takeScreenShot()
         if len(file_list) >= 100:
@@ -25,4 +26,46 @@ def watch(delay,boolean):
     time.sleep(delay)
 
 watch(0,True)
+'''
 
+import time
+import pyautogui
+import os
+
+folder_path = 'F:\GitHub\python-learning\python\hello world\screenCapture\ScreenShots'
+
+
+
+def takeScreenShot(file_list):
+    screenshot = pyautogui.screenshot()
+    screenshot.save(os.path.join(folder_path, f'ScreenShot{len(file_list)}.png'))
+
+
+def watch(delay, boolean):
+
+    while boolean:
+        file_list = os.listdir(folder_path)
+        if len(file_list) > 10:
+            #file_list.sort(key=lambda x: os.path.getmtime(os.path.join(folder_path, x)))
+            os.remove(os.path.join(folder_path, file_list[-1]))
+            print("Removed oldest screenshot")
+
+        else:
+            takeScreenShot(file_list)
+            print("Not removed")
+        time.sleep(delay)
+
+def deleteScreenshots(folder_path):
+    file_list = os.listdir(folder_path)
+    for file_name in file_list:
+        if file_name.endswith('.png'):
+            file_path = os.path.join(folder_path, file_name)
+            os.remove(file_path)
+
+    print("All screenshots have been deleted.")
+
+
+
+
+watch(1, True)
+#deleteScreenshots(folder_path)
