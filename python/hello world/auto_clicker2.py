40ab8eceaaa85147c62697288d3585f467023abb
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-10-13 14:53:23 -0600

    Update auto_clicker2.py

--- python/hello world/auto_clicker2.py
+++ python/hello world/auto_clicker2.py
@@ -1,3 +1,4 @@
+'''
 from pynput import keyboard
 import time
 
@@ -6,7 +7,7 @@ delay_between_press = .01
 time_till_stop = 1
 def on_press(key):
     print(key)
-    if key == keyboard.Key.backspace:
+    if key == keyboard.Key.space:
 
         press_enter(repeat_times, delay_between_press, time_till_stop)
 
@@ -29,6 +30,53 @@ def press_enter(num_presses, delay, duration):
             time.sleep(delay)
 
             keyboard.Controller().release(keyboard.Key.enter)
+
+
+
+# Create a listener
+
+listener = keyboard.Listener(on_press=on_press)
+
+# Start the listener
+
+listener.start()
+
+# Wait for the listener to stop (e.g., by pressing Ctrl+C)
+
+listener.join()
+
+'''
+from pynput import keyboard
+import time
+
+repeat_times = 100
+delay_between_press = .01
+time_till_stop = 1
+def on_press(key):
+    print(key)
+    if key == keyboard.Key.caps_lock:
+
+        press_enter(repeat_times, delay_between_press, time_till_stop)
+
+
+
+def press_enter(num_presses, delay, duration):
+
+    start_time = time.time()
+
+    end_time = start_time + duration
+
+
+
+    while time.time() < end_time:
+
+        for _ in range(num_presses):
+
+            keyboard.Controller().press("w")
+
+            time.sleep(delay)
+
+            keyboard.Controller().release("w")
 
 
 
